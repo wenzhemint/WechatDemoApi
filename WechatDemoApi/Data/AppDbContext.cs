cc95266681e66083a98aa19e1e27a03dce26af9d
Jason WEN <wenzhemint@outlook.com>
2026-04-25 14:23:52 +0800

    add model mapping(optional).

--- WechatDemoApi/Data/AppDbContext.cs
+++ WechatDemoApi/Data/AppDbContext.cs
@@ -11,4 +11,25 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.OpenId)
+                .HasColumnName("OpenId")
+                .IsRequired()
+                .HasMaxLength(100);
+            entity.Property(e => e.Username)
+                .IsRequired()
+                .HasMaxLength(100);
+            entity.Property(e => e.Role)
+                .HasMaxLength(20)
+                .HasDefaultValue("User");
+            entity.Property(e => e.CreatedAt)
+                .ValueGeneratedOnAdd();
+        });
+    }
 }
